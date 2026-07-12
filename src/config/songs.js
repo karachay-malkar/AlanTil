b8f1ce9d627dceca56bd8f4379b394a978debcb3
taulu <taulantau5@gmail.com>
2026-07-12 19:52:23 +0300

    Update songs.js

--- src/config/songs.js
+++ src/config/songs.js
@@ -1,3 +1,3 @@
-export const SONGS_SHEET_URL = "";
+export const SONGS_SHEET_URL = "https://docs.google.com/spreadsheets/d/1aQseG2yQfeZiAbxX0dmhnoP7IkoR4w-N/edit?usp=drivesdk&ouid=111397706846712470648&rtpof=true&sd=true";
 export const SONGS_CACHE_KEY = "fc_songs_cache_v1";
 export const SONGS_CACHE_VERSION = 1;
