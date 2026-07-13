dd229fc673ce478a71668b694b7b050fd2a691bd
taulu <taulantau5@gmail.com>
2026-07-13 10:43:57 +0300

    11.7

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -1,4 +1,4 @@
 export const measurementId = "G-1WSMD45Q9D";
-export const appVersion = "11.6";
+export const appVersion = "11.7";
 export const analyticsEnabled = true;
 export const debugMode = new URLSearchParams(window.location.search).get("analytics_debug") === "1";
