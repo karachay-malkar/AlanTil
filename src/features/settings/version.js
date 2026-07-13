dd889f2f48b2865733ba52ae4430b59d759ef67c
taulu <taulantau5@gmail.com>
2026-07-13 16:10:07 +0300

    11.8: version screen

--- src/features/settings/version.js
+++ src/features/settings/version.js
@@ -5,10 +5,7 @@ export function renderVersion(context) {
     title: "Версия приложения",
     body: `
       <dl class="settingsFacts">
-        <div><dt>Версия</dt><dd>11.7</dd></div>
-        <div><dt>Тип приложения</dt><dd>SPA</dd></div>
-        <div><dt>Аналитика</dt><dd>Google Analytics 4</dd></div>
-        <div><dt>Идентификатор</dt><dd>G-1WSMD45Q9D</dd></div>
+        <div><dt>Версия</dt><dd>11.8</dd></div>
         <div><dt>Последнее обновление</dt><dd>июль 2026</dd></div>
       </dl>`,
   });
