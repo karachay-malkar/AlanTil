7ef7f117a95eba47ad2d29587cb4d4c7f42a9c47
taulu <taulantau5@gmail.com>
2026-07-13 16:09:25 +0300

    11.8: privacy config

---
+++ src/config/privacy.js
@@ -0,0 +1,15 @@
+export const ANALYTICS_PREFERENCE_KEY = "alantil_analytics_enabled_v1";
+export const PRIVACY_REGION_CACHE_KEY = "alantil_privacy_region_v1";
+export const PRIVACY_REGION_ENDPOINT = "https://ipapi.co/country_code/";
+export const PRIVACY_REGION_TIMEOUT_MS = 1800;
+
+export const CONSENT_REQUIRED_COUNTRIES = Object.freeze([
+  "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
+  "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
+  "PL", "PT", "RO", "SK", "SI", "ES", "SE", "IS", "LI", "NO",
+  "GB", "CH",
+]);
+
+export const CIS_COUNTRIES = Object.freeze([
+  "RU", "BY", "KZ", "AM", "KG", "UZ", "AZ", "TJ", "TM", "MD",
+]);
