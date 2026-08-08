a85c6337a17e2dc00e3ff51cba2dc39280e63c23
taulu <taulantau5@gmail.com>
2026-08-09 02:05:09 +0300

    13.13 use hierarchical content view

--- src/config/words.js
+++ src/config/words.js
@@ -1,11 +1,12 @@
-export const DICTIONARY_CACHE_KEY = "alantil_dictionary_cache_v3";
+export const DICTIONARY_CACHE_KEY = "alantil_dictionary_cache_v4";
 export const LEGACY_DICTIONARY_CACHE_KEYS = Object.freeze([
   "fc_words_cache_v30",
   "alantil_dictionary_cache_v1",
   "alantil_dictionary_cache_v2",
+  "alantil_dictionary_cache_v3",
 ]);
 export const DICTIONARY_METADATA_TABLE = "dictionary_metadata";
-export const DICTIONARY_CONTENT_TABLE = "content_words";
+export const DICTIONARY_CONTENT_TABLE = "v_words_app";
 export const DICTIONARY_STORIES_TABLE = "content_stories";
 export const DICTIONARY_KEY = "main";
 
