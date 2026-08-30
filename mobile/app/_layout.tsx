841b21a7e296a50cb83cccddf60abdfcc889b756
taulu <taulantau5@gmail.com>
2026-08-30 11:58:24 +0300

    14.1.1: order auth before cloud settings

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -9,14 +9,15 @@ import { theme } from '@/src/mobile/theme';
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SettingsProvider>
-        <SessionProvider>
+      <SessionProvider>
+        <SettingsProvider>
           <StatusBar style="dark" backgroundColor={theme.colors.background} />
           <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: theme.colors.background } }}>
             <Stack.Screen name="index" />
+            <Stack.Screen name="(tabs)" />
           </Stack>
-        </SessionProvider>
-      </SettingsProvider>
+        </SettingsProvider>
+      </SessionProvider>
     </SafeAreaProvider>
   );
 }
