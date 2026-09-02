ac7c396685baae6ec65574553f59d472ff727d9f
taulu <taulantau5@gmail.com>
2026-09-02 19:29:50 +0300

    16.4: make Web use shared dictionary contract

--- src/config/words.js
+++ src/config/words.js
@@ -1,15 +1,11 @@
-export const DICTIONARY_CACHE_KEY = "alantil_dictionary_cache_v5";
-export const LEGACY_DICTIONARY_CACHE_KEYS = Object.freeze([
-  "fc_words_cache_v30",
-  "alantil_dictionary_cache_v1",
-  "alantil_dictionary_cache_v2",
-  "alantil_dictionary_cache_v3",
-  "alantil_dictionary_cache_v4",
-]);
-export const DICTIONARY_METADATA_TABLE = "dictionary_metadata";
-export const DICTIONARY_CONTENT_TABLE = "v_words_app";
-export const DICTIONARY_STORIES_TABLE = "content_stories";
-export const DICTIONARY_KEY = "main";
+export {
+  DICTIONARY_CACHE_KEY,
+  LEGACY_DICTIONARY_CACHE_KEYS,
+  DICTIONARY_METADATA_TABLE,
+  DICTIONARY_CONTENT_TABLE,
+  DICTIONARY_STORIES_TABLE,
+  DICTIONARY_KEY,
+} from "../../packages/alantil-core/dictionary-contract.js";
 
 export const DICT_TITLES = Object.freeze({});
 export const SECTION_TITLES = Object.freeze({});
