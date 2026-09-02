2bc54d948c14eed3636e3e162ea8c3d449186002
taulu <taulantau5@gmail.com>
2026-09-02 06:21:25 +0300

    16.0: align Metro with Expo defaults

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -6,5 +6,4 @@ const workspaceRoot = path.resolve(projectRoot, '..');
 const config = getDefaultConfig(projectRoot);
 config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
-config.resolver.disableHierarchicalLookup = true;
 module.exports = config;
